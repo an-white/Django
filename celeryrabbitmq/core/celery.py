add999d4acf513708f781ed3ce62d306a7af610e
An_White <andresblancahernandez@gmail.com>
2022-02-07 16:30:47 -0400

    created a basic mailing app with Django, Celery and RabbitMQ

--- celeryrabbitmq/core/celery.py
+++ celeryrabbitmq/core/celery.py
@@ -4,9 +4,12 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "celeryrabbitmq.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
 
-app = Celery("celeryrabbitmq")
+"""
+nombre de la instancia de celery 'core'
+"""
+app = Celery("core")
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
